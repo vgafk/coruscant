c42fb5cfdc40036631dbf0477f02ccbbc3a63662
Yartsev Vladimir <v.rainovsky@yandex.ru>
2022-11-02 18:14:57 +0300

    initial commit

---
+++ coruscant/coruscant/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coruscant project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coruscant.settings')
+
+application = get_wsgi_application()
